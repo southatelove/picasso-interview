cfbb3c5091ddb12ed46c0cd5f8e44020a37ec99b
southatelove <artem.jlez@gmail.com>
2023-11-26 01:19:49 +0300

    fix-scroll-prob

--- src/components/PostsCard/PostsCard.tsx
+++ src/components/PostsCard/PostsCard.tsx
@@ -1,16 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./PostsCard.module.css";
 import { PostsCardProps } from "./PostsCard.props";
-import Button from "../Button/Button";
 
 export function PostsCard(props: PostsCardProps) {
-  const navigate = useNavigate();
-
-  const cardInfo = () => {
-    navigate(`/posts/${props.id}`);
-  };
   return (
-    <div className={styles["link"]}>
+    <Link to={`/posts/${props.id}`} className={styles["link"]}>
       <div className={styles["info"]}>
         <p className={styles["text-posts"]}>№</p>&nbsp;
         <div className={styles["userId"]}>{props.userId}</div>
@@ -23,7 +17,6 @@ export function PostsCard(props: PostsCardProps) {
         <p className={styles["text-posts"]}>Описание:</p>&nbsp;
         <span className={styles["description"]}>{props.body}</span>
       </div>
-      <Button onClick={cardInfo}>Подробнее</Button>
-    </div>
+    </Link>
   );
 }
